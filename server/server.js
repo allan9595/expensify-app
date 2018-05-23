4c9ca7b0829a090b3ddea115f3596e3ec0e5c40e
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-05-23 16:34:48 -0500

    set up production service

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000 ; //use heroku dynamic env port number
+const port = process.env.PORT || 3000; //use heroku dynamic env port number
 app.use(express.static(publicPath)); //create a express app
 app.get('*',(req,res) => {
   res.sendFile(path.join(publicPath,'index.html'));
