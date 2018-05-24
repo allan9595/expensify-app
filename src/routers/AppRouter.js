136e5a2db5415673463487c06388be12929c09dd
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-05-24 00:47:35 -0500

    set up production service

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import ExpenseDashBoardPage from '../components/ExpenseDashboardPage';
+import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
