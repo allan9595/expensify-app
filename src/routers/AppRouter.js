c16f26f2a23b8f9c92226be27229a47802666018
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-05-28 01:04:42 -0500

    minor fix

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,16 +7,16 @@ import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
-import PrivteRoute from './PrivteRoute';
+import PrivateRoute from './PrivateRoute';
 export const history = createHistory();
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <PrivteRoute path="/dashboard" component={ExpenseDashboardPage} />
-        <PrivteRoute path="/create" component={AddExpensePage} />
-        <PrivteRoute path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
